9177a23a7d5b0f95deecf549448b5a7114d32b9a
Develeon64 <software@develeon.de>
2022-07-14 00:14:36 +0200

    Added personal Library to Playlist Action

--- Views/PlaylistActionConfigView.cs
+++ Views/PlaylistActionConfigView.cs
@@ -21,22 +21,28 @@ namespace Develeon64.SpotifyPlugin.Views {
 			this.lblPlaylist.Text = PluginLanguageManager.PluginStrings.PlaylistActionPlaylist;
 			this.lblTrackNumber.Text = PluginLanguageManager.PluginStrings.PlaylistActionTrack;
 
+			this.cbxPlaylist.Items.Add("Library");
+
 			this.playlists = await SpotifyHelper.GetPlaylists();
-			foreach (SimplePlaylist playlist in playlists)
+			this.playlists.Sort((x, y) => x.Name.CompareTo(y.Name));
+			foreach (SimplePlaylist playlist in this.playlists)
 				this.cbxPlaylist.Items.Add(playlist.Name);
-			this.cbxPlaylist.SelectedIndex = this.playlists.FindIndex(p => p.Uri == this._viewModel.Configuration.Uri);
+			this.cbxPlaylist.SelectedIndex = this.playlists.FindIndex(p => p.Uri == this._viewModel.Configuration.Uri) + 1;
 			this.nupTrackNumber.Value = this._viewModel.Configuration.Track + 1;
 		}
 
 		public override bool OnActionSave () {
 			this._viewModel.Configuration.Name = this.cbxPlaylist.SelectedItem as string;
-			this._viewModel.Configuration.Uri = this.playlists[this.cbxPlaylist.SelectedIndex].Uri;
+			this._viewModel.Configuration.Uri = this.cbxPlaylist.SelectedIndex == 0 ? "Library" : this.playlists[this.cbxPlaylist.SelectedIndex - 1].Uri;
 			this._viewModel.Configuration.Track = (int)this.nupTrackNumber.Value - 1;
 			return this._viewModel.SaveConfig();
 		}
 
-		private void CbxPlaylist_SelectedIndexChanged (object sender, EventArgs e) {
-			this.nupTrackNumber.Maximum = this.playlists[this.cbxPlaylist.SelectedIndex].Tracks.Total ?? int.MaxValue;
+		private async void CbxPlaylist_SelectedIndexChanged (object sender, EventArgs e) {
+			if (this.cbxPlaylist.SelectedIndex > 0)
+				this.nupTrackNumber.Maximum = this.playlists[this.cbxPlaylist.SelectedIndex - 1].Tracks.Total ?? int.MaxValue;
+			else
+				this.nupTrackNumber.Maximum = (await SpotifyHelper.GetLibraryTracks()).Count;
 		}
 	}
 }
